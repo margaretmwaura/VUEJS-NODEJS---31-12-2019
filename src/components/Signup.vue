330e6662e5dcb54e6d41eb326b6506a25c7030c1
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-01-01 16:58:39 +0300

    configuring backend for adding of users

---
+++ src/components/Signup.vue
@@ -0,0 +1,57 @@
+<template>
+    <input v-model="firstname">
+    <input v-model="lastname">
+    <input v-model="password">
+    <button @click="create">Add post</button>
+</template>
+
+<script>
+    import axios from "axios";
+    export default {
+        data() {
+            return {
+                firstname : " ",
+                lastname : " ",
+                password: " ",
+            }
+        },
+        methods: {
+            create() {
+                axios
+                    .post('http://localhost:4000/signup',['firstname','lastname','password'])
+                    .then(response => {
+                        var code = response.status;
+                        console.log(code);
+                        if(code === 200)
+                        {
+                            this.$notify({
+                                group: 'foo',
+                                title: 'Success',
+                                text: 'Posting of data was successful'
+                            });
+                        }
+                        else
+                        {
+                            this.$notify({
+                                group: 'foo',
+                                title: 'Failure',
+                                text: 'Error occured when posting data : error code : ' + code
+                            });
+                        }
+                    })
+                    .catch(error => {
+                        this.$notify({
+                            group: 'foo',
+                            title: 'Failure',
+                            text: error
+                        });
+                    })
+                    .finally(() => this.loading = false)
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
